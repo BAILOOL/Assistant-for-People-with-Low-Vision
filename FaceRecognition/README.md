2917f9391f84c7cef688a6cb949c42c84dc84875
Oleg <oyurchenko145@gmail.com>
2017-10-10 21:52:41 +0300

    FaceRecognition: add readme, comments

---
+++ FaceRecognition/README.md
@@ -0,0 +1,9 @@
+# Face Recognition module
+
+Description of important files in this directory:
+```
+recognition.py              	# main recognition class
+recognition                    	# derictory with model for OpenFace
+data		   	        # directory containing saved embeddings (simple database)
+frontalization			# main frontalization logic
+```
--- FaceRecognition/recognition.py
+++ FaceRecognition/recognition.py
@@ -19,8 +19,8 @@ from sklearn.naive_bayes import GaussianNB
 
 import pdb
 
-PATH = '/home/oleg/recognition/faces/Arnold-Schwarzenegger-Family-Pictures-Wife-Son-Daughter-Age.jpg'
-
+#example image
+PATH = 'img.jpg'
 
 class FaceRecognition(object):
 	def __init__(self, threshold, detection_net):
@@ -43,38 +43,30 @@ class FaceRecognition(object):
 		
 
 	def recognize(self, img):
-		#pdb.set_trace()
+		# detect faces on the image
 		boxes = self.detectionNN.detect_Face(img)
 		faces = []
 
-
+		# crop detected faces
 		for box in boxes:
 			faces.append(img[box[1]:box[3], box[0]:box[2]])
 
 		labels = []
 		
 		for face in faces:
-			print(face.shape)
-			
+			# large images cause strange bug,
+			# therefore, images should be resized (96 by 96).
+			# we tend to save proportions of the image,
+			# in order to avoid distortions in embeddings		
 			smallFace = face
-
 			resize_factor = 1
-
 			if (smallFace.shape[0] > smallFace.shape[1]):
 				resize_factor = 96. / smallFace.shape[1]
 			else:
 				resize_factor = 96. / smallFace.shape[0]
-
 			smallFace = cv2.resize(smallFace, (int(smallFace.shape[1] * resize_factor), int(smallFace.shape[0] * resize_factor)))
 
-			#cv2.imshow("Image", smallFace)
-			#cv2.waitKey(0)
-
 			Frontalized = self.frontalization.frontalize(smallFace)
-
-			cv2.imwrite("/home/oleg/server_assembly/recognition/Image.jpg", Frontalized)
-			#cv2.waitKey(0)
-
 
 			cropedAlignedfaces = self.detectionNN.detect_Face(Frontalized)
 			if len(cropedAlignedfaces) == 0:
@@ -83,23 +75,18 @@ class FaceRecognition(object):
 			box = cropedAlignedfaces[0]
 			cropedAligned = Frontalized[box[1]:box[3], box[0]:box[2]]
 
-			#cv2.imshow("Image", cropedAligned)
-			#cv2.imwrite("/home/oleg/cropped.png", cropedAligned)
-			#cv2.waitKey(0)
-
 			face96 = cv2.resize(cropedAligned, (96, 96))
-			#cv2.imshow("Image", face96)
-			#cv2.waitKey(0)
 
 			face96RGB = cv2.cvtColor(face96.astype(np.uint8), cv2.COLOR_BGR2RGB)
 
 			rep = self.recognitionNN.forward(face96RGB)
 			labels.append(self.match(rep))
-		
 
 		return labels
 
 	def match(self, frep):
+		# iterate over existing embeddings in database
+		# and match the best one using L2 norm
 		best = 10
 		best_i = 0;
 		for i, rep in enumerate(self.embeddings):
@@ -108,7 +95,6 @@ class FaceRecognition(object):
 				best = val
 				best_i = i
 
-		print("Distance: " + str(best))
 		if best > self.threshold:
 			return "I don't know :("
 
@@ -116,6 +102,8 @@ class FaceRecognition(object):
 
 
 	def add(self, img, name, img_name):
+		# adding people to database.
+		# logic is similar to recognition
 		faces = self.detectionNN.detect_Face(img)
 		if (len(faces) > 1):
 			return "too many people! must be 1!"
@@ -142,8 +130,6 @@ class FaceRecognition(object):
 		cropedAligned = Frontalized[cropedAlignedBox[1]:cropedAlignedBox[3], cropedAlignedBox[0]:cropedAlignedBox[2]]
 
 		face96 = cv2.resize(cropedAligned, (96, 96))
-		#cv2.imshow("Image", face96)
-		#cv2.waitKey(0)
 
 		face96RGB = cv2.cvtColor(face96, cv2.COLOR_BGR2RGB)
 
@@ -153,46 +139,22 @@ class FaceRecognition(object):
 		cv2.imwrite(fileName, cropedAligned)
 
 		tmp = pd.DataFrame([[fileName, name]], columns=['picture', 'name'])
-		print("labels before")
-		print(self.labels)
-		print("new label")
-		print(tmp)
 		if self.labels.empty:
-			print("label was empty - overwriting")
 			self.labels = tmp
 		else:
-			print("appending labels")
-			#self.labels.append(tmp, ignore_index=True)
 			self.labels = pd.concat([self.labels, tmp], ignore_index=True)
-		print("new labels")
-		print(self.labels)
 
 		rep.shape = (1, rep.size)
-		print("embeddings before")
-		print(self.embeddings)
-		print(self.embeddings.shape)
-		print("new embeddings")
-		print(rep)
 		if self.embeddings.size == 0:
-			print("embeddings were empty - overwriting")
 			self.embeddings = rep
 		else:
-			print("appending embeddings")
 			self.embeddings = np.append(self.embeddings, rep, axis=0)
 
-		print("new")
-		print(self.embeddings)
-		print(self.labels)
 		# save values
 		self.labels.to_csv(self.labelsPath, columns=["picture", 'name'], index=False)
 		np.savetxt(self.embeddingsPath, self.embeddings, delimiter=",")
 
 		return "success"
-
-
-
-
-		
 
 if __name__ == "__main__":
 	faceRecognizer = FaceRecognition(1.1)
